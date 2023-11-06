1e182edc91f35823170813c94665b9a6c625ce20
Travis Callahan <travis.callahan@allata.com>
2023-11-06 09:48:15 -0600

    Add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import LatestIssues from "./LatestIssues";
 import Pagination from "./components/Pagination";
 import IssueChart from "./IssueChart";
 import { Flex, Grid } from "@radix-ui/themes";
+import { Metadata } from "next";
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
@@ -21,3 +22,8 @@ export default async function Home() {
     </Grid>
   );
 }
+
+export const metadata: Metadata = {
+  title: "Issue Tracker - Dashboard",
+  description: "View a summary of project issues",
+};
